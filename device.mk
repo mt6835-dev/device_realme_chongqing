f95ad37112dc62be34ca022637280b57534acbec
JaswantTeja <sunnyteja201@gmail.com>
2025-06-30 08:45:30 +0530

    chongqing: Set shipping API level to 33 (Android 13)
    Change-Id: Ic702729888084ba3b8a96f522a2026aff5ce6996
    Signed-off-by: JaswantTeja <sunnyteja201@gmail.com>

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Declare device shipping API level (Android 13)
+PRODUCT_SHIPPING_API_LEVEL := 33
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
